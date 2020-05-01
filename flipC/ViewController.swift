2516fe1a2dd2138b2aadadac76a5bafd70387192
Abrish <abrishsabri97@gmail.com>
2020-05-01 21:06:51 +0200

    Display 16 card cells

--- flipC/ViewController.swift
+++ flipC/ViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
 
     let model = CardModel()
     var cardsArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
         cardsArray = model.getCards()
+
+        // set the view controller as the datasource and delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+
+
+    // MARK: - Collection View Delegate Methods
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        //return number of cards
+        return cardsArray.count
+     }
+
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // Get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+
+        // Configure cell
+
+
+        // return cell
+        return cell
+     }
 
 
 
